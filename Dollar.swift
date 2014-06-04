c732b2fcf25bbccbbe2177b97445eb5c777ba660
Ankur Patel <ankur.patel@ymail.com>
2014-06-03 22:16:53 -0400

    Adding Compact and Flatten

--- Dollar.swift
+++ Dollar.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct $ {
     
-    //LoDash.first([0, 1, false, 2, '', 3]);
+    //$.first([0, 1, false, 2, '', 3]);
     //Gets the first element or first n elements of an array.
     static func first(collection: AnyObject[]) -> AnyObject {
         if collection.isEmpty {
@@ -20,7 +20,43 @@ struct $ {
         }
     }
     
-    //LoDash.noop()
+    //$.compact([0, 1, false, 2, '', 3])
+    //Remove all falsey values
+    static func compact(array: AnyObject[]) -> AnyObject[] {
+        var index = -1
+        let length = array.count
+        var result: AnyObject[] = []
+    
+        while ++index < length {
+            if let value : AnyObject = AnyObject?(array[index]) {
+                result += value
+            }
+        }
+        return result
+    }
+
+    //$.flatten([[2],3,4])
+    //flatten array
+    static func flatten(array: AnyObject[], isShallow: Bool) -> AnyObject[] {
+        return self.flatten(array, isShallow: isShallow, fromIndex: 0)
+    }
+    
+    //Private
+    static func flatten(array: AnyObject[], isShallow: Bool, fromIndex: Int) -> AnyObject[] {
+        let length = array.count
+        if length == 0 {
+            return []
+        }
+        var index = fromIndex - 1
+        var result: AnyObject[] = [];
+        
+        while ++index < length {
+            //Todo
+        }
+        return result;
+    }
+
+    //$.noop()
     //A no-operation function.
     static func noop() -> AnyObject {
         return NSNull();
