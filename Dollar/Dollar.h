06ee87d71041fffced28fd118938ef9c46611997
Ankur Patel <ankur.patel@ymail.com>
2014-06-09 19:36:08 -0400

    Initial Commit

---
+++ Dollar/Dollar.h
@@ -0,0 +1,19 @@
+//
+//  Dollar.h
+//  Dollar
+//
+//  Created by Ankur Patel on 6/9/14.
+//  Copyright (c) 2014 Encore Dev Labs LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Dollar.
+FOUNDATION_EXPORT double DollarVersionNumber;
+
+//! Project version string for Dollar.
+FOUNDATION_EXPORT const unsigned char DollarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dollar/PublicHeader.h>
+
+
