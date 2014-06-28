60704fb078e46ceaa1273a07671cf1880ba4cd00
Ankur Patel <ankur.patel@ymail.com>
2014-06-28 19:13:09 -0400

    Adding cent project

---
+++ Cent/CentTests/CentTests.swift
@@ -0,0 +1,13 @@
+//
+//  CentTests.swift
+//  CentTests
+//
+//  Created by Ankur Patel on 6/28/14.
+//  Copyright (c) 2014 Encore Dev Labs LLC. All rights reserved.
+//
+
+import XCTest
+
+class CentTests: XCTestCase {
+    
+}
