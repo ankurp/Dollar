807360b4b7213fabbdb5638e6951b68ddd93a61f
Ankur Patel <ankur.patel@ymail.com>
2014-10-16 22:40:01 -0400

    Creating iOS target for test

--- Cent/CentTests/CentTests.swift
+++ Cent/CentTests/CentTests.swift
@@ -2,13 +2,21 @@
 //  CentTests.swift
 //  CentTests
 //
-//  Created by Ankur Patel on 6/28/14.
+//  Created by Ankur Patel on 10/16/14.
 //  Copyright (c) 2014 Encore Dev Labs LLC. All rights reserved.
 //
 
+import UIKit
 import XCTest
-import Cent
 
 class CentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
 
 }
