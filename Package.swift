db352b0b1a4c90353fa5eb591f1588515d81bc9b
Spencer Schoeben <spencer@netspencer.com>
2019-10-01 14:38:56 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
-// swift-tools-version:5.0
-
+// swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(name: "Dollar")
+let package = Package(
+    name: "Dollar",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(name: "Dollar", targets: ["Dollar"]),
+    ],
+    targets: [
+        .target(name: "Dollar", path: "Sources")
+    ]
+)
